d5869d07e7d39e2aaeba1fed0a88cfed7f99fd1c
shotarotakagi <tasypo080@gmail.com>
2015-03-13 12:18:21 +0900

    add cpp class template

---
+++ Cocos2d-x_v3/C++Class.xctemplate/___FILEBASENAME___.cpp
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+using namespace cocos2d;
+
+___FILEBASENAMEASIDENTIFIER___::___FILEBASENAMEASIDENTIFIER___()
+{
+
+}
+
+___FILEBASENAMEASIDENTIFIER___::~___FILEBASENAMEASIDENTIFIER___()
+{
+
+}
---
+++ Cocos2d-x_v3/C++Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+class ___FILEBASENAMEASIDENTIFIER___
+{
+public:
+
+protected:
+    ___FILEBASENAMEASIDENTIFIER___();
+    virtual ~___FILEBASENAMEASIDENTIFIER___();
+
+private:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
