5bee43afe3ef45d16f336723c39a06af3ca9be81
ShotaroTakagi <tasypo080@gmail.com>
2015-05-18 15:22:43 +0900

    fix parent

--- Cocos2d-x_v3/NodeClass.xctemplate/___FILEBASENAME___.h
+++ Cocos2d-x_v3/NodeClass.xctemplate/___FILEBASENAME___.h
@@ -11,7 +11,7 @@
 
 #include "cocos2d.h"
 
-class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::Sprite
+class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::Node
 {
 public:
     CREATE_FUNC(___FILEBASENAMEASIDENTIFIER___);
